e49d61657a287306eb333e7814c3f0f87c5ce189
joeCodes05 <nathancodes05@gmail.com>
2023-11-26 10:39:36 +0100

    project compeleted - worked on responsive design for both tablets and mobile devices.

--- src/pages/technology/technology.jsx
+++ src/pages/technology/technology.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { techData } from './constansts/techData'
-import { isMobile } from 'react-device-detect'
 
 const Technology = () => {
   const [currentStep, setCurrentStep] = useState(0);
@@ -17,36 +16,56 @@ const Technology = () => {
     },
   ]
 
+  const [width, setWidth] = useState(window.innerWidth);
+  const handleWindowSizeChange = () => {
+    setWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowSizeChange);
+    return () => {
+      window.removeEventListener('resize', handleWindowSizeChange);
+    }
+  }, []);
+
   return (
     <>
       <section className="overflow-hidden">
-        <div className="h-screen mx-auto w-full lg:bg-[url('../images/technology/background-technology-desktop.jpg')] md:bg-[url('../images/technology/background-technology-tablet.jpg')] bg-[url('../images/technology/background-technology-mobile.jpg')] bg-cover bg-no-repeat flex items-center">
+        <div className="h-screen mx-auto w-full lg:bg-[url('../images/technology/background-technology-desktop.jpg')] md:bg-[url('../images/technology/background-technology-tablet.jpg')] bg-[url('../images/technology/background-technology-mobile.jpg')] bg-cover bg-no-repeat flex lg:flex-row flex-col lg:items-center justify-center lg:justify-normal">
+          <h5
+            data-aos="fade-in"
+            data-aos-duration="2000"
+            className="text-white flex lg:hidden md:text-left gap-5 md:justify-normal justify-center md:text-[25px] leading-[18px] text-lg font-barlow tracking-widest uppercase md:ml-10 text-[23px]"
+          >
+            <div className="text-gray-400">03</div>
+            <div>Space launch 101</div>
+          </h5>
           {techData.map((data, index) => {
             const { title, detail, desktopImageUrl, mobileImageUrl } = data;
 
             return (
               <div key={index}>
-                {currentStep === index ? <div key={index} className="flex lg:flex-row flex-col-reverse items-center">
+                {currentStep === index ? <div key={index} className="flex lg:flex-row flex-col-reverse md:items-center">
                   <div className="basis-2/3">
-                    <div className="w-[65%] h-full mx-auto">
+                    <div className="md:w-[65%] w-[98%] h-full mx-auto">
                       <h5
                         data-aos="fade-in"
                         data-aos-duration="2000"
-                        className="text-white md:text-left flex gap-5 md:justify-normal justify-center md:text-[25px] md:leading-[18px] text-lg font-barlow tracking-widest uppercase"
+                        className="text-white hidden lg:flex md:text-left gap-5 md:justify-normal justify-center md:text-[25px] md:leading-[18px] text-lg font-barlow tracking-widest uppercase"
                       >
                         <div className="text-gray-400">03</div>
                         <div>Space launch 101</div>
                       </h5>
 
-                      <div className="mt-28">
-                        <div className="flex gap-10">
+                      <div className="lg:mt-28 md:mt-10 mt-5">
+                        <div className="flex lg:flex-row flex-col lg:items-start items-center lg:text-left text-center lg:gap-10 gap-5">
                           <div>
-                            <div className="flex flex-col gap-7">
+                            <div className="flex lg:flex-col flex-row lg:gap-7 gap-3">
                               {buttonData.map((button, index) => {
                                 const { buttonInnerText } = button;
 
                                 return (
-                                  <button key={index} onClick={() => setCurrentStep(index)} className={currentStep === index ? 'h-[60px] w-[60px] rounded-full text-gray-900 bg-white border flex items-center justify-center text-2xl font-bellefair border-gray-600 duration-700 hover:border-white' : 'h-[60px] w-[60px] rounded-full text-white border flex items-center justify-center text-2xl font-bellefair border-gray-600 duration-700 hover:border-white'}>
+                                  <button key={index} onClick={() => setCurrentStep(index)} className={currentStep === index ? 'md:h-[60px] md:w-[60px] w-[50px] h-[50px] rounded-full text-gray-900 bg-white border flex items-center justify-center text-2xl font-bellefair border-gray-600 duration-700 hover:border-white' : 'md:h-[60px] md:w-[60px] w-[50px] h-[50px] rounded-full text-white border flex items-center justify-center text-2xl font-bellefair border-gray-600 duration-700 hover:border-white'}>
                                     {buttonInnerText}
                                   </button>
                                 )
@@ -71,8 +90,7 @@ const Technology = () => {
                             </h1>
 
                             <p
-
-                              className="mt-5 lg:text-left text-center text-gray-400 md:text-lg text-base font-barlow font-light"
+                              className="md:mt-5 mt-2 lg:text-left text-center text-gray-400 md:text-lg text-base font-barlow font-light"
                             >
                               {detail}
                             </p>
@@ -81,8 +99,8 @@ const Technology = () => {
                       </div>
                     </div>
                   </div>
-                  <div className="basis-1/3 mt-[100px]">
-                    <img src={isMobile ? mobileImageUrl : desktopImageUrl} alt="adaf" />
+                  <div className="basis-1/3 lg:mt-[100px] md:mt-[70px] mt-[50px]">
+                    <img src={width <= 768 ? mobileImageUrl : desktopImageUrl} alt={title} />
                   </div>
                 </div> : null}
               </div>
